ba6291c319e9b1f22a74f355ae2456fcde4e883c
Anthony Martin <anthony.ct.martin@gmail.com>
2020-09-29 06:37:50 -0700

    Pull in code from https://github.com/anthony-c-martin/bicep-types-poc

---
+++ src/autorest.azureresourceschema/Models/ResourceDefinition.cs
@@ -0,0 +1,16 @@
+﻿using AutoRest.Core.Model;
+using Bicep.SerializedTypes.Concrete;
+
+namespace AutoRest.AzureResourceSchema.Models
+{
+    public class ResourceDefinition
+    {
+        public ResourceDescriptor Descriptor { get; set; }
+
+        public Method DeclaringMethod { get; set; }
+
+        public Method GetMethod { get; set; }
+
+        public ResourceType Type { get; set; }
+    }
+}
